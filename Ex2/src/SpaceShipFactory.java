555724eb030155dff967ff4f3afe05b959a5fb1c
Tomer Nissim <tomer.nissim@mail.huji.ac.il>
2020-11-08 15:29:44 +0200

    Updated the Factory class to include returning null and counting the nubmer of human ships.

--- Ex2/src/SpaceShipFactory.java
+++ Ex2/src/SpaceShipFactory.java
@@ -9,28 +9,37 @@ public class SpaceShipFactory {
      */
     public static SpaceShip[] createSpaceShips(String[] args) {
         SpaceShip[] list = new SpaceShip[args.length];
+        int humanCount = 0;
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("b")) {
                 list[i] = new BasherShip();
-            } else {
-                if (args[i].equals("r")) {
-                    list[i] = new RunnerShip();
-                } else {
-                    if (args[i].equals("d")) {
-                        list[i] = new DrunkardShip();
-                    } else {
-                        if (args[i].equals("h")) {
-                            list[i] = new HumanControlledShip();
-                        } else {
-                            if (args[i].equals("a")) {
-                                list[i] = new AggressiveShip();
-                            } else {
-                                list[i] = new SpecialShip();
-                            }
-                        }
-                    }
-                }
+                continue;
             }
+            if (args[i].equals("r")) {
+                list[i] = new RunnerShip();
+                continue;
+            }
+            if (args[i].equals("d")) {
+                list[i] = new DrunkardShip();
+                continue;
+            }
+            if (args[i].equals("h")) {
+                list[i] = new HumanControlledShip();
+                humanCount++;
+                continue;
+            }
+            if (args[i].equals("a")) {
+                list[i] = new AggressiveShip();
+                continue;
+            }
+            if (args[i].equals("s")) {
+                list[i] = new SpecialShip();
+                continue;
+            }
+            return null;
+        }
+        if(humanCount < 1){
+            return null;
         }
         return list;
     }
