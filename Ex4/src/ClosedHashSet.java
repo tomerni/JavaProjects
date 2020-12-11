e7df25aa2174955b1c89c447e2124967bb29662c
Tomer Nissim <tomer.nissim@mail.huji.ac.il>
2020-12-11 10:50:44 +0200

    changed the deletion method - using compare by reference

--- Ex4/src/ClosedHashSet.java
+++ Ex4/src/ClosedHashSet.java
@@ -4,140 +4,146 @@
 public class ClosedHashSet extends SimpleHashSet {
 
 
-	/** the hash table */
-	public Object[] hashTable;
+    /** the hash table */
+    public String[] hashTable;
 
-	// CONSTRUCTORS
+    // this string represents a deleted string
+    private final String deletedString = "deleted";
 
-	/**
-	 * A default constructor. Constructs a new, empty table with default initial capacity (16), upper load
-	 * factor (0.75) and lower load factor (0.25).
-	 */
-	public ClosedHashSet() {
-		super();
-		hashTable = new Object[INITIAL_CAPACITY];
-	}
+    // CONSTRUCTORS
 
-	/**
-	 * Constructs a new, empty table with the specified load factors, and the default initial capacity (16).
-	 * @param upperLoadFactor The upper load factor of the hash table.
-	 * @param loweLoadFactor The lower load factor of the hash table.
-	 */
-	public ClosedHashSet(float upperLoadFactor, float loweLoadFactor) {
-		super(upperLoadFactor, loweLoadFactor);
-		hashTable = new Object[INITIAL_CAPACITY];
-	}
+    /**
+     * A default constructor. Constructs a new, empty table with default initial capacity (16), upper load
+     * factor (0.75) and lower load factor (0.25).
+     */
+    public ClosedHashSet() {
+        super();
+        hashTable = new String[INITIAL_CAPACITY];
+    }
 
-	/**
-	 * Data constructor - builds the hash set by adding the elements one by one. Duplicate values should be
-	 * ignored. The new table has the default values of initial capacity (16), upper load factor (0.75), and
-	 * lower load factor (0.25).
-	 * @param data Values to add to the set.
-	 */
-	public ClosedHashSet(String[] data) {
-		super();
-		hashTable = new Object[INITIAL_CAPACITY];
-		for (String s : data) {
-			add(s);
-		}
-	}
+    /**
+     * Constructs a new, empty table with the specified load factors, and the default initial capacity (16).
+     * @param upperLoadFactor The upper load factor of the hash table.
+     * @param loweLoadFactor The lower load factor of the hash table.
+     */
+    public ClosedHashSet(float upperLoadFactor, float loweLoadFactor) {
+        super(upperLoadFactor, loweLoadFactor);
+        hashTable = new String[INITIAL_CAPACITY];
+    }
 
-	// PRIVATE METHODS
+    /**
+     * Data constructor - builds the hash set by adding the elements one by one. Duplicate values should be
+     * ignored. The new table has the default values of initial capacity (16), upper load factor (0.75), and
+     * lower load factor (0.25).
+     * @param data Values to add to the set.
+     */
+    public ClosedHashSet(String[] data) {
+        super();
+        hashTable = new String[INITIAL_CAPACITY];
+        for (String s : data) {
+            add(s);
+        }
+    }
 
-	/*
-	 * Finds empty space in the table and adds the value
-	 * @param newValue New value to add to the set
-	 */
-	private void findSpaceAndAdd(Object newValue, Object[] hashTable) {
-		for (int i = 0; i < hashTable.length; i++) {
-			int index = clamp(newValue.hashCode() + (i + i * i) / 2);
-			if (hashTable[index] == null | hashTable[index] instanceof DeletedString) {
-				hashTable[index] = newValue;
-				return;
-			}
-		}
-	}
+    // PRIVATE METHODS
 
-	/*
-	 * Deletes the given item from the table (the item is in the table for sure)
-	 * @param toDelete Value to delete
-	 */
-	private void deleteCurValue(String toDelete) {
-		for (int i = 0; i < curCapacity; i++) {
-			int index = clamp(toDelete.hashCode() + (i + i * i) / 2);
-			if (hashTable[index].equals(toDelete)) {
-				hashTable[index] = new DeletedString(toDelete);
-				return;
-			}
-		}
-	}
+    /*
+     * Finds empty space in the table and adds the value
+     * @param newValue New value to add to the set
+     */
+    private void findSpaceAndAdd(Object newValue, Object[] hashTable) {
+        for (int i = 0; i < hashTable.length; i++) {
+            int index = clamp(newValue.hashCode() + (i + i * i) / 2);
+            if (hashTable[index] == null | hashTable[index] == deletedString) {
+                hashTable[index] = newValue;
+                return;
+            }
+        }
+    }
 
-	/*
-	 * re-hashes the values in the table after resizing
-	 */
-	private void reHash() {
-		Object[] newHashTable = new Object[curCapacity];
-		for (Object s : hashTable) {
-			if (s != null & !(s instanceof DeletedString)) {
-				findSpaceAndAdd(s, newHashTable);
-			}
-		}
-		hashTable = newHashTable;
-	}
+    /*
+     * Deletes the given item from the table (the item is in the table for sure)
+     * @param toDelete Value to delete
+     */
+    private void deleteCurValue(String toDelete) {
+        for (int i = 0; i < curCapacity; i++) {
+            int index = clamp(toDelete.hashCode() + (i + i * i) / 2);
+            if (hashTable[index].equals(toDelete)) {
+                hashTable[index] = deletedString;
+                return;
+            }
+        }
+    }
 
-	// PUBLIC AND PROTECTED METHODS
+    /*
+     * re-hashes the values in the table after resizing
+     */
+    private void reHash() {
+        String[] newHashTable = new String[curCapacity];
+        for (String s : hashTable) {
+            if (s != null & s != deletedString) {
+                findSpaceAndAdd(s, newHashTable);
+            }
+        }
+        hashTable = newHashTable;
+    }
 
-	/**
-	 * Add a specified element to the set if it's not already in it.
-	 * @param newValue New value to add to the set
-	 * @return False iff newValue already exists in the set
-	 */
-	public boolean add(String newValue) {
-		if (contains(newValue)) {
-			return false;
-		}
-		curNumOfItems++;
-		if ((float) curNumOfItems / curCapacity > upperFactor) {
-			curCapacity *= 2;
-			reHash();
-		}
-		findSpaceAndAdd(newValue, hashTable);
-		return true;
-	}
+    // PUBLIC AND PROTECTED METHODS
 
-	/**
-	 * Look for a specified value in the set.
-	 * @param searchVal Value to search for
-	 * @return True iff searchVal is found in the set
-	 */
-	public boolean contains(String searchVal) {
-		for (int i = 0; i < curCapacity; i++) {
-			int index = clamp(searchVal.hashCode() + (i + i * i) / 2);
-			if (hashTable[index] == null) {
-				return false;
-			}
-			if (hashTable[index].equals(searchVal)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    /**
+     * Add a specified element to the set if it's not already in it.
+     * @param newValue New value to add to the set
+     * @return False iff newValue already exists in the set
+     */
+    public boolean add(String newValue) {
+        if (contains(newValue)) {
+            return false;
+        }
+        curNumOfItems++;
+        if ((float) curNumOfItems / curCapacity > upperFactor) {
+            curCapacity *= 2;
+            reHash();
+        }
+        findSpaceAndAdd(newValue, hashTable);
+        return true;
+    }
 
-	/**
-	 * Remove the input element from the set.
-	 * @param toDelete Value to delete
-	 * @return True iff toDelete is found and deleted
-	 */
-	public boolean delete(String toDelete) {
-		if (!contains(toDelete)) {
-			return false;
-		}
-		curNumOfItems--;
-		if ((float) curNumOfItems / curCapacity < lowerFactor) {
-			curCapacity /= 2;
-			reHash();
-		}
-		deleteCurValue(toDelete);
-		return true;
-	}
+    /**
+     * Look for a specified value in the set.
+     * @param searchVal Value to search for
+     * @return True iff searchVal is found in the set
+     */
+    public boolean contains(String searchVal) {
+        for (int i = 0; i < curCapacity; i++) {
+            int index = clamp(searchVal.hashCode() + (i + i * i) / 2);
+            if (hashTable[index] == deletedString) {
+                continue;
+            }
+            if (hashTable[index] == null) {
+                return false;
+            }
+            if (hashTable[index].equals(searchVal)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Remove the input element from the set.
+     * @param toDelete Value to delete
+     * @return True iff toDelete is found and deleted
+     */
+    public boolean delete(String toDelete) {
+        if (!contains(toDelete)) {
+            return false;
+        }
+        curNumOfItems--;
+        if ((float) curNumOfItems / curCapacity < lowerFactor) {
+            curCapacity /= 2;
+            reHash();
+        }
+        deleteCurValue(toDelete);
+        return true;
+    }
 }
