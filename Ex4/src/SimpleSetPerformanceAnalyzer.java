87a97f89b34d4d601ff5517f3de74b598f03fe37
Tomer Nissim <tomer.nissim@mail.huji.ac.il>
2020-12-11 17:42:07 +0200

    added main method to the analyzer

--- Ex4/src/SimpleSetPerformanceAnalyzer.java
+++ Ex4/src/SimpleSetPerformanceAnalyzer.java
@@ -2,104 +2,102 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.TreeSet;
 
-import org.junit.*;
-
 /**
  * implements the SimpleSetPerformanceAnalyzer class
  */
 public class SimpleSetPerformanceAnalyzer {
 
-	// the number of warmup iterations
-	private static final int WARMUP_ITERATIONS = 70000;
+    // the number of warmup iterations
+    private static final int WARMUP_ITERATIONS = 70000;
 
-	// the number of data structures to analyze
-	private static final int NUM_OF_DATA_STRUCTURES = 5;
+    // the number of data structures to analyze
+    private static final int NUM_OF_DATA_STRUCTURES = 5;
 
-	// data1 string array
-	private static final String[] data1 = Ex4Utils.file2array("src\\data1.txt");
+    // data1 string array
+    private static final String[] data1 = Ex4Utils.file2array("src\\data1.txt");
 
-	// data2 string array
-	private static final String[] data2 = Ex4Utils.file2array("src\\data2.txt");
+    // data2 string array
+    private static final String[] data2 = Ex4Utils.file2array("src\\data2.txt");
 
-	// array with the data structures for data1
-	private final SimpleSet[] dataSets1;
+    // array with the data structures for data1
+    private static SimpleSet[] dataSets1 = new SimpleSet[NUM_OF_DATA_STRUCTURES];
 
-	// array with the data structures for data2
-	private final SimpleSet[] dataSets2;
+    // array with the data structures for data2
+    private static SimpleSet[] dataSets2 = new SimpleSet[NUM_OF_DATA_STRUCTURES];
 
-	// CONSTRUCTOR
+    // CONSTRUCTOR
 
-	/**
-	 * constructs the arrays for the analyze
-	 */
-	public SimpleSetPerformanceAnalyzer() {
-		dataSets1 = new SimpleSet[NUM_OF_DATA_STRUCTURES];
-		dataSets2 = new SimpleSet[NUM_OF_DATA_STRUCTURES];
-		initDataSet(dataSets1);
-		initDataSet(dataSets2);
-	}
+    /**
+     * constructs the arrays for the analyze
+     */
+    public SimpleSetPerformanceAnalyzer() {
+    }
 
-	// PRIVATE METHODS
+    // PUBLIC METHODS
 
-	/*
-	 * init the given array with the data structures
-	 */
-	private void initDataSet(SimpleSet[] s) {
-		s[1] = new ClosedHashSet();
-		s[0] = new OpenHashSet();
-		s[4] = new CollectionFacadeSet(new LinkedList<String>());
-		s[2] = new CollectionFacadeSet(new TreeSet<String>());
-		s[3] = new CollectionFacadeSet(new HashSet<String>());
-	}
+    /**
+     * executes the test. To disable a test just comment it (//)
+     * @param args command prompt args
+     */
+    public static void main(String[] args) {
+        initDataSet(dataSets1);
+        initDataSet(dataSets2);
+        addData(dataSets1, data1);
+        containsCheck("hi", dataSets1);
+        containsCheck("-13170890158", dataSets1);
+        addData(dataSets2, data2);
+        containsCheck("23", dataSets2);
+        containsCheck("hi", dataSets2);
+    }
 
-	/*
-	 * Adds the data from the string array to the data structures in the set
-	 */
-	private void addData(SimpleSet[] set, String[] data) {
-		for (int i = 0; i < NUM_OF_DATA_STRUCTURES; i++) {
-			long timeBefore = System.nanoTime();
-			assert data != null;
-			for (String s : data) {
-				set[i].add(s);
-			}
-			long difference = System.nanoTime() - timeBefore;
-			System.out.println(difference / 1000000);
-		}
-	}
+    // PRIVATE METHODS
 
-	/*
-	 * performs a contain check for the given word in the given set
-	 */
-	private void containsCheck(String word, SimpleSet[] set) {
-		long timeBefore = 0;
-		for (int i = 0; i < NUM_OF_DATA_STRUCTURES - 1; i++) {
-			for (int j = 0; j < 2 * WARMUP_ITERATIONS; j++) {
-				set[i].contains(word);
-				if (j == WARMUP_ITERATIONS) {
-					timeBefore = System.nanoTime();
-				}
-			}
-			long difference = System.nanoTime() - timeBefore;
-			System.out.println(word + " " + difference / WARMUP_ITERATIONS);
-		}
-		timeBefore = System.nanoTime();
-		for (int j = 0; j < (WARMUP_ITERATIONS / 10); j++) {
-			set[4].contains(word);
-		}
-		long difference = System.nanoTime() - timeBefore;
-		System.out.println(word + " " + difference / (WARMUP_ITERATIONS / 10));
-	}
+    /*
+     * init the given array with the data structures
+     */
+    private static void initDataSet(SimpleSet[] s) {
+        s[1] = new ClosedHashSet();
+        s[0] = new OpenHashSet();
+        s[4] = new CollectionFacadeSet(new LinkedList<String>());
+        s[2] = new CollectionFacadeSet(new TreeSet<String>());
+        s[3] = new CollectionFacadeSet(new HashSet<String>());
+    }
 
-	/*
-	 * executes the tests
-	 */
-	@Test
-	public void dataTest() {
-		addData(dataSets1, data1);
-		containsCheck("hi", dataSets1);
-		containsCheck("-13170890158", dataSets1);
-		addData(dataSets2, data2);
-		containsCheck("23", dataSets2);
-		containsCheck("hi", dataSets2);
-	}
+    /*
+     * Adds the data from the string array to the data structures in the set
+     */
+    private static void addData(SimpleSet[] set, String[] data) {
+        for (int i = 0; i < NUM_OF_DATA_STRUCTURES; i++) {
+            long timeBefore = System.nanoTime();
+            assert data != null;
+            for (String s : data) {
+                set[i].add(s);
+            }
+            long difference = System.nanoTime() - timeBefore;
+            System.out.println(difference / 1000000);
+        }
+    }
+
+    /*
+     * performs a contain check for the given word in the given set
+     */
+    private static void containsCheck(String word, SimpleSet[] set) {
+        long timeBefore = 0;
+        for (int i = 0; i < NUM_OF_DATA_STRUCTURES - 1; i++) {
+            for (int j = 0; j < 2 * WARMUP_ITERATIONS; j++) {
+                set[i].contains(word);
+                if (j == WARMUP_ITERATIONS) {
+                    timeBefore = System.nanoTime();
+                }
+            }
+            long difference = System.nanoTime() - timeBefore;
+            System.out.println(word + " " + difference / WARMUP_ITERATIONS);
+        }
+        timeBefore = System.nanoTime();
+        for (int j = 0; j < (WARMUP_ITERATIONS / 10); j++) {
+            set[4].contains(word);
+        }
+        long difference = System.nanoTime() - timeBefore;
+        System.out.println(word + " " + difference / (WARMUP_ITERATIONS / 10));
+    }
 }
